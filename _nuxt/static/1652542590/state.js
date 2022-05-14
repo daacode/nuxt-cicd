9200e55c78c71f4dc00bb7a1c00fb1e5ec690f81
daacode <adeneyeabiodun@gmail.com>
2022-05-14 15:36:39 +0000

    [skip ci] Updates

---
+++ _nuxt/static/1652542590/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-cicd\u002F_nuxt\u002Fstatic\u002F1652542590",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fnuxt-cicd\u002F",assetsPath:"\u002Fnuxt-cicd\u002F_nuxt\u002F",cdnURL:a}}}}(null));
